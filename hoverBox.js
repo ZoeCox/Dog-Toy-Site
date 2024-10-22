88bbd6cc8c8fb74636b7689f9a83df6e7bdbdf68
ZoeCox <zoemcox16@gmail.com>
2024-10-22 17:28:12 +0100

    layout updates

--- hoverBox.js
+++ hoverBox.js
@@ -1,10 +1,24 @@
 const hoverText = document.querySelector(".hover-text");
 const introBlock = document.querySelector(".intro-block");
 
+const toyBox1 = document.querySelectorAll(".toy1");
+const toyBox2 = document.querySelectorAll(".toy2");
+const toyBox3 = document.querySelectorAll(".toy3");
+const toyBox4 = document.querySelectorAll(".toy4");
+
+const toy1Text = document.querySelectorAll(".toy1-text");
+const toy2Text = document.querySelectorAll(".toy2-text");
+const toy3Text = document.querySelectorAll(".toy3-text");
+const toy4Text = document.querySelectorAll(".toy4-text");
+
 introBlock.addEventListener("mouseover", () => {
   hoverText.classList.remove("hidden");
 });
 
 introBlock.addEventListener("mouseup", () => {
   hoverText.classList.add("hidden");
+});
+
+toyBox1.addEventListener("click", () => {
+  toy1Text.innerHTML = "hello";
 });
